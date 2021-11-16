480e07dbb31976b7f843ff1c7129edc917fd70fa
Pavel Saroka <pavel_saroka@epam.com>
2021-11-16 22:00:50 +0300

    test console program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using TestGeneratorLib;
+using FileInfo = TestGeneratorLib.FileInfo;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string code = File.ReadAllText("C:/Users/Pavel_Saroka/RiderProjects/TestGenerator/TestGeneratorLib/ClassLoader.cs");
+            FileInfo fileInfo = ClassLoader.GetFileInfo(code);
+            var generatedTests = TestGenerator.GenerateTests(fileInfo);
+            Console.WriteLine(generatedTests);
+        }
+    }
+}
